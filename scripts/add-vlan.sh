8eef95af8790797865a1bbb04425c9567c7b49fb
Andy Bavier <acb@cs.princeton.edu>
2015-04-09 18:17:14 -0400

    Fixes

--- scripts/add-vlan.sh
+++ scripts/add-vlan.sh
@@ -3,10 +3,10 @@
 IFACE=$1
 VLAN=$2
 
-ifconfig $IFACE.$VLAN 2&>1 > /dev/null
+ifconfig "$IFACE"."$VLAN" 2>&1 > /dev/null
 if [ "$?" -ne 0 ]
 then
-    ifconfig $IFACE
-    vconfig add $IFACE $VLAN
-    ifconfig $IFACE.$VLAN up
+    ifconfig "$IFACE" up
+    vconfig add "$IFACE" "$VLAN"
+    ifconfig "$IFACE"."$VLAN" up
 fi
